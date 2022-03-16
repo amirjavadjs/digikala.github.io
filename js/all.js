6287bb23f6522cf8daad4122b8b92540cb2bcf6b
amir javadi <amirjavadi1386@gmail.com>
2022-03-16 06:39:14 -0700

    login page my ficher nummber

--- js/all.js
+++ js/all.js
@@ -19,3 +19,18 @@ var swiper = new Swiper('.swiper', {
     return direction;
   }
   
+
+
+  function show(){
+    var inputtest = document.getElementById("input").value;
+    var text =document.getElementById("ptext");
+    var text1 =document.getElementById("ptext1");
+    if(inputtest===""||inputtest===null){
+  return text.innerHTML="لطفا شماره خود را وارد کنید"
+    
+  }
+  if(typeof inputtest==="string")
+  {
+    text1.innerHTML="شماره را درست وارد کن"
+  }
+}
